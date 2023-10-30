3b4abd7482b059c9e6decbf14fc9fbc0d88cc02e
TURKCELL\TCABUYUKHATIPOGLU <arda.buyukhatipoglu@turkcell.com.tr>
2023-10-30 11:48:20 +0300

    finished except downloads tab

--- netflix/Managers/APICaller.swift
+++ netflix/Managers/APICaller.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 struct Constants {
-    static let API_KEY = "Your Movie Db Api Key"
+    static let API_KEY = "1140c7fa704f458e35437eb5d4c42375"
     static let baseURL = "https://api.themoviedb.org"
-    static let YoutubeAPI_KEY = "Your youtube api key"
+    static let YoutubeAPI_KEY = "AIzaSyA74pjOqfqMdyRkPt4BJyULidRvZcTNo4o"
     static let YoutubeBaseUrl = "https://youtube.googleapis.com/youtube/v3/search?"
 }
 
